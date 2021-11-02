65eb66d0e43fc19b49cdfb728ced0a106a2a354a
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2021-11-02 09:32:48 +0100

    SQL: javadoc

--- E-Banking/src/sample/SQL.java
+++ E-Banking/src/sample/SQL.java
@@ -10,6 +10,10 @@ public class SQL {
     private Statement statement;
     private ResultSet rs;
 
+    /**
+     * This methode calls the DB and make the connection.
+     * The Try-catch catch all exeptions (from DB conncetion) and print them out.
+     */
     public SQL() {
         try {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
@@ -19,6 +23,22 @@ public class SQL {
         }
     }
 
+    /**
+     *This methode is used to login.
+     * First, data is taken from the DB via the connection.
+     * Afterwards these are compared with the user inputs.
+     * If they match, the user is logged in and receives his data.
+     *
+     * @param contractNumber
+     * The contractnumber contains the number that the user has entered.
+     * This number corresponds to his contract number in the Db.
+     * @param password
+     * The contractnumber contains a string that the user has entered.
+     * This number corresponds to his password in the Db.
+     *
+     * @return
+     * The userdata gets returnt or null if the user logging failed.
+     */
     public User login(String contractNumber, String password) {
         try {
             statement = connection.createStatement();
@@ -37,6 +57,26 @@ public class SQL {
         return null;
     }
 
+    /**
+     * This methode is used to register a new user.
+     *
+     * @param firstName
+     * Contains the firstname that the user has entered.
+     * @param lastName
+     * Contains the lastname that the user has entered.
+     * @param eMail
+     * Contains the email that the user has entered.
+     * @param address
+     * Contains the address that the user has entered.
+     * @param birthDate
+     * Contains the birthdate that the user has entered.
+     * @param password
+     * Contains the password that the user has entered.
+     *
+     * @return
+     * If everything is fine a new user gets returnt.
+     * If there are some complications null gets returnt.
+     */
     public User register(String firstName, String lastName, String eMail, String address, Date birthDate, String password) {
         if (!firstName.isBlank() && !lastName.isBlank() && !eMail.isBlank() && !address.isBlank() && !password.isBlank()) {
             try {
@@ -53,6 +93,15 @@ public class SQL {
         return null;
     }
 
+    /**
+     *In this methode the user gets his own conctractnumber.
+     * Every user have a unique number.
+     * The contractnumbers of all users are taken from the db.
+     * After that a new contractnumber will be created and it will be checked that this number does not exist yet.
+     *
+     * @return
+     * The contractnumber gets return
+     */
     private String createContractNumber() {
         ArrayList<Integer> existingContractNumbers = new ArrayList<>();
         String contractNumber = "";
